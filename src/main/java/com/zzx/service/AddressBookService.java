09cbcb92ea27d9a0e93c8b480835fbe4ec2616b8
泽希 <2287952503@qq.com>
2023-05-16 20:55:53 +0800

    回复没写后端除登录的代码之前

--- src/main/java/com/zzx/service/AddressBookService.java
+++
@@ -1,9 +0,0 @@
-package com.zzx.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.zzx.entity.AddressBook;
-import com.zzx.entity.User;
-
-public interface AddressBookService extends IService<AddressBook> {
-
-}
