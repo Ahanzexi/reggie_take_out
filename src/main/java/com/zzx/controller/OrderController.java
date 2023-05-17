1e65d4a3f8f879fa8b13b93978fdfd20840fb63d
泽希 <2287952503@qq.com>
2023-05-17 19:44:39 +0800

    提交订单成功

--- src/main/java/com/zzx/controller/OrderController.java
+++ src/main/java/com/zzx/controller/OrderController.java
@@ -1,11 +1,15 @@
 package com.zzx.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.zzx.common.R;
 import com.zzx.entity.Orders;
 import com.zzx.service.OrderService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpSession;
 
 @Slf4j
 @RequestMapping("/order")
@@ -22,5 +26,15 @@ public class OrderController {
     public R<String> submit(@RequestBody Orders order){
         orderService.submit(order);
         return R.success("下单成功");
+    }
+    /*http://localhost:8080/order/userPage?page=1&pageSize=5*/
+    @GetMapping("/userPage")
+    public R<Page> page(Integer page, Integer pageSize, HttpSession session){
+        final Page pageInfo = new Page(page, pageSize);
+        final LambdaQueryWrapper<Orders> wrapper = new LambdaQueryWrapper<>();
+        wrapper.eq(Orders::getUserId,session.getAttribute("user"));
+        wrapper.orderByDesc(Orders::getOrderTime);
+        orderService.page(pageInfo, wrapper);
+        return R.success(pageInfo);
     }
 }
